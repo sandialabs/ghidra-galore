0e6e9a37bb430e4922647108913cad4be3cb3aed
Jon Crussell <jcrusse@sandia.gov>
2025-03-31 12:11:14 -0700

    fix copyrights

--- ghidra_scripts/PrintMetrics.java
+++ ghidra_scripts/PrintMetrics.java
@@ -1,16 +1,19 @@
-/* ###
- * NOTICE: This computer software was prepared by National Technology &
- * Engineering Solutions of Sandia, LLC, hereinafter the Contractor, under
- * Contract DE-NA0003525 with the Department of Energy/National Nuclear
- * Security Administration (DOE/NNSA). All rights in the computer software are
- * reserved by DOE/NNSA on behalf of the United States Government and the
- * Contractor as provided in the Contract. You are authorized to use this
- * computer software for Governmental purposes but it is not to be released or
- * distributed to the public.
- *
- * NEITHER THE GOVERNMENT NOR THE CONTRACTOR MAKES ANY WARRANTY, EXPRESS OR
- * IMPLIED, OR ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE. This notice
- * including this sentence must appear on any copies of this computer software.
+/*
+ * Copyright 2025 National Technology & Engineering Solutions of Sandia, LLC
+ * (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the U.S.
+ * Government retains certain rights in this software.
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ *     http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 // Dump some simple metrics about the analysis
